8e5d2c263837c6dd1bd04b201230624d7e6e5710
Trevor <reevest2@gmail.com>
2026-02-16 15:27:19 -0500

    required

---
+++ Models/Resources/ResourceBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Models.Resources;
+
+public class ResourceBase<T>
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string EntityId { get; set; }
+    public string OwnerId { get; set; }
+    public string SubjectId { get; set; }
+    public int Version { get; set; }
+    public DateTime CreatedAt { get; set; } 
+    public DateTime? UpdatedAt { get; set; } 
+    public bool IsDeleted { get; set; }
+    public required T Data { get; set; }
+}
