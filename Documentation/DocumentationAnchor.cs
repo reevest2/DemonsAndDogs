3b71b5cc9cee1252afc162822d0b6045da5f8fb7
Trevor <reevest2@gmail.com>
2026-03-14 13:35:03 -0400

    remove bloat

--- Documentation/DocumentationAnchor.cs
+++
@@ -1,7 +0,0 @@
-namespace Documentation;
-
-/// <summary>
-/// Marker class used by MCPServer to locate the Documentation assembly.
-/// Do not remove.
-/// </summary>
-public static class DocumentationAnchor { }
